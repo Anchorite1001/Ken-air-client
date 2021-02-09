2debdba08cc76534097eac47f42a57abc2548222
Mai Nguyen <chimai.dk@gmail.com>
2021-02-10 10:59:11 +1100

    Add router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,61 @@
-import axios from 'axios'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/logout">Logout</Link>
+            </li>
+            <li>
+              <Link to="/airplanes">Planes</Link>
+            </li>
+            <li>
+              <Link to="/flights">Flights</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
 
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <About />
+          </Route>
+          <Route path="/logout">
+            <Users />
+          </Route>
+          <Route path="airplanes">
+            <Home />
+          </Route>
+          <Route path="flights">
+            <Home />
+          </Route>
+          <Route path="search">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/components/ShortFlightInfo.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class ShortFlightInfo extends Component {
+  render() {
+    return(
+      <div>
+        Flight info coming soon
+      </div>
+    )
+  }
+}
+
+export default ShortFlightInfo;
