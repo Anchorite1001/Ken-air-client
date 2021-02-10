0337922c705ee6557855e37ebbaa3d38af8abf97
Anchorite1001 <yangwu.1415@gmail.com>
2021-02-10 14:44:24 +1100

    change to ken's version of app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
-=======
 import axios from 'axios'
-// import PlaneCreate from './PlaneCreate'
->>>>>>> b4dbf76a3975e32a0c15bf81c6790a54fc701299
 import React from "react";
 import PlaneCreate from './PlaneCreate';
 import Login from './Login';
 import Logout from './Logout';
 import FlightsList from './FlightsList';
 import BookSeatApp from './BookSeatApp';
+import SearchFlight from './SearchFlight'
 
 
 import {
@@ -17,14 +14,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-<<<<<<< HEAD
-=======
 
-import FlightsList from './FlightsList'
-import SearchFlight from './SearchFlight'
-import BookSeatApp from './BookSeatApp'
-
->>>>>>> b4dbf76a3975e32a0c15bf81c6790a54fc701299
 
 function App() {
   return (
@@ -65,13 +55,6 @@ function App() {
           </Route>
 
           <Route path="/airplanes">
-<<<<<<< HEAD
-            <PlaneCreate />
-          </Route>
-
-          <Route path="flight/:id">
-            /* <BookSeatApp /> */
-=======
             /* Your own Class*/
           </Route>
 
@@ -81,11 +64,10 @@ function App() {
 
           <Route path="/search">
             <SearchFlight />
-          </Route>  
+          </Route>
 
           <Route path="/flight/:id">
             <BookSeatApp />
->>>>>>> b4dbf76a3975e32a0c15bf81c6790a54fc701299
           </Route>
 
           <Route path="search">
