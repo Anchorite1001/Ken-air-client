0dc3b459dc8b96157f8a17ae562dab2c334e1e5b
Mai Nguyen <chimai.dk@gmail.com>
2021-02-10 14:14:46 +1100

    Add some more file to get rid of error

--- src/components/BookSeatApp.js
+++ src/components/BookSeatApp.js
@@ -1,13 +1,32 @@
+import {useParams} from "react-router-dom"
+import axios from 'axios'
 import ShortFlightInfo from './ShortFlightInfo';
+import SeatsBoard from './SeatsBoard'
+import SelectSeatBoard from './SelectSeatBoard'
 
-function BookSeatApp() {
-  return (
-    <div>
-      <ShortFlightInfo />
-      <SeatsBoard />
-      <SelectSeatBoard />
-    </div>
-  )
+const SERVER_URL = 'http://localhost:3000/flights.json'
+
+class BookSeatApp extends Component {
+  const fetchId = (id) => {
+    id = useParams();
+  }
+
+  const fetchFlightInfo = () => {
+    axios.get(SERVER_URL).then((response) => {
+      console.log(response)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>flight_id: {id}</p>
+        <ShortFlightInfo flight_id={id} />
+        <SeatsBoard flight_id={id} />
+        <SelectSeatBoard />
+      </div>
+    )
+  }
 }
 
 export default BookSeatApp;
