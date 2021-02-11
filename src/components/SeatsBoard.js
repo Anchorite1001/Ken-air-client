7b0c763dfc80c9b78c580e0c0319216ca809362a
Mai Nguyen <chimai.dk@gmail.com>
2021-02-11 12:16:56 +1100

    Added BookSeatApp and SeatsBoard from last night

--- src/components/SeatsBoard.js
+++ src/components/SeatsBoard.js
@@ -6,84 +6,80 @@ import '../index.css';
 function Square(props) {
   return (
     <button
-      className="square"
-      onClick={() => props.onClick()}>
-      {props.value}
+      // className="square"
+      onClick={() => props.seat.onClick(props.seat.id)}>
+      {props.seat.id}
     </button>
   )
 }
 
-function GenerateBoard(rows, cols) {
-  const generateLettersArray = (numOfLetters) => {
-    const allLetters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
-    return allLetters.slice(0, numOfLetters);
-  }
-  const colArray = generateLettersArray(cols);
-  const rowArray = [...Array(rows).keys()].map(x => ++x);
-
-  const allSeats = [];
-  rowArray.forEach((number) => {
-    let tempArray = []
-    colArray.forEach((letter) => {
-      tempArray.push(letter + number)
-    })
-    allSeats.push(tempArray);
-  });
-  console.log(allSeats);
-  return allSeats;
+function Row(props) {
+  return (
+    <div className="board-row">
+      {props.seats.map((seat, index) => {
+        return <Square key={index} seat={seat} />
+      })}
+    </div>
+  )
 }
 
+function Board(props) {
+  return (
+    props.rows.map((seats) => {
+      return <Row seats={seats} />
+    })
+  )
+}
 
 
 class SeatsBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: [],
+      allSeats: {},
+
     }
+    console.log(props.rows);
+    console.log(props.cols);
   }
 
   _handleClick(i) {
-    const squares = this.state.squares.slice();
-    // if (calculateWinner(squares) || squares[i]) {
-    //   return;
-    // }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
+  }
+
+  GenerateBoard(rows, cols, flight_id) {
+    const generateLettersArray = (numOfLetters) => {
+      const allLetters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+      return allLetters.slice(0, numOfLetters);
+    }
+    const colArray = generateLettersArray(parseInt(cols));
+    const rowArray = [...Array(parseInt(rows)).keys()].map(x => ++x);
+
+    const allSeats = [];
+    console.log(this.props);
+    rowArray.forEach((number) => {
+      let rowArray = [];
+      colArray.forEach((letter) => {
+        let seatObj = {
+          id: (letter + number),
+          flight_id: flight_id,
+          user_id: '',
+          onClick: (seat_id) => this.props.onClick(seat_id)
+        };
+        rowArray.push(seatObj);
+      })
+      allSeats.push(rowArray);
     });
-  }
-
-  renderSquare(i) {
-    return <Square
-            value={this.state.squares[i]}
-            onClick={() => this._handleClick(i)}
-    />;
-  }
-
-  renderBoard(array) {
-    return
-      array.map((row, i) => {
-        console.log(row)
-        <div className="board-row">
-          {row.map((seat, i) => {
-            <Square
-              value={seat}
-              onClick={() => this._handleClick(seat)}
-            />;
-          })}
-        </div>
-      });
+    console.log(allSeats);
+    return allSeats;
   }
 
 
   render() {
     return (
       <div>
-        {this.renderBoard([['A1', 'B1', 'C1'], ['A2', 'B2', 'C2']])}
+        <Board rows={this.GenerateBoard(this.props.rows, this.props.cols, this.props.flight_id)} />
       </div>
-    );
+    )
   }
 }
 
